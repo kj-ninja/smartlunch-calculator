f8ad371b8cb063f826086a75472ddab6096c777b
kj-ninja <jakubiak.krzy@gmail.com>
2021-06-08 10:52:16 +0200

    deploy

---
+++ precache-manifest.e495f8a680fda5f91f5de1cc8ff97df0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d3952e516c33c627d7",
+    "url": "/smartlunch-calculator/css/app.cc3ef6ea.css"
+  },
+  {
+    "revision": "9fb58c9d208993d6ad8f",
+    "url": "/smartlunch-calculator/css/calculator.66e53651.css"
+  },
+  {
+    "revision": "f497c35401b6a773b82ea0f133cb37fe",
+    "url": "/smartlunch-calculator/img/grupa.f497c354.svg"
+  },
+  {
+    "revision": "ede023c2f601cbb69f6199fefcab24c9",
+    "url": "/smartlunch-calculator/index.html"
+  },
+  {
+    "revision": "58d3952e516c33c627d7",
+    "url": "/smartlunch-calculator/js/app.efd9d37c.js"
+  },
+  {
+    "revision": "9fb58c9d208993d6ad8f",
+    "url": "/smartlunch-calculator/js/calculator.4b6afb70.js"
+  },
+  {
+    "revision": "707b06b499e6f5bd3bec",
+    "url": "/smartlunch-calculator/js/chunk-vendors.a8962bfc.js"
+  },
+  {
+    "revision": "1b100fc14d1d57be45f8883941e00798",
+    "url": "/smartlunch-calculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/smartlunch-calculator/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/smartlunch-calculator/precache-manifest.e495f8a680fda5f91f5de1cc8ff97df0.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "smartlunch-calculator"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
